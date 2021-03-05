b2882abb645775cf0589fe0d61dbbeec4d5cd439
vagrant <vagrant@shopware.local>
2021-03-05 08:04:57 +0000

    Add Definitions for Step and StepTypes

---
+++ src/DAL/Step/Aggregate/StepTypeDefinition.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Sg\ProdConfigurator\DAL\Step\Aggregate;
+
+use Sg\ProdConfigurator\DAL\Step\StepDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\ReverseInherited;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+
+class StepTypeDefinition extends EntityDefinition
+{
+    public const ENTITY_NAME = 'sg_step_type';
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
+            (new StringField('technical_name', 'technicalName'))->addFlags(new Required()),
+            (new StringField('label', 'label'))->addFlags(new Required()),
+            (new OneToManyAssociationField('steps', StepDefinition::class, 'step_type_id', 'id'))
+                ->addFlags(new ReverseInherited('stepType'))
+        ]);
+    }
+
+}
---
+++ src/DAL/Step/StepDefinition.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Sg\ProdConfigurator\DAL\Step;
+
+use Sg\ProdConfigurator\DAL\Step\Aggregate\StepTypeDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Inherited;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
+use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+
+class StepDefinition extends EntityDefinition
+{
+    public const ENTITY_NAME = 'sg_step';
+
+    public function getEntityName(): string
+    {
+        return self::ENTITY_NAME;
+    }
+
+    protected function defineFields(): FieldCollection
+    {
+        return new FieldCollection([
+            (new IdField('id', 'id'))->addFlags(new PrimaryKey(), new Required()),
+            (new StringField('technical_name', 'technicalName'))->addFlags(new Required()),
+            (new StringField('label', 'label'))->addFlags(new Required()),
+            new LongTextField('description', 'description'),
+            (new ManyToOneAssociationField('stepType', 'step_type_id', StepTypeDefinition::class, 'id'))
+                ->addFlags(new Inherited())
+            ]);
+    }
+
+}
