e098a2e2f15a2f7f76860c00bb6908a7e022040e
vagrant <vagrant@shopware.local>
2021-03-08 15:58:25 +0000

    Finish StepType admin UI

--- src/Resources/app/administration/src/module/sg-prod-step-type/index.js
+++ src/Resources/app/administration/src/module/sg-prod-step-type/index.js
@@ -1,8 +1,8 @@
 const { Module } = Shopware;
 
 import './page/sg-prod-step-type-index';
-//import './page/sg-prod-step-type-detail';
-//import './page/sg-prod-step-type-create';
+import './page/sg-prod-step-type-detail';
+import './page/sg-prod-step-type-create';
 
 Module.register('sg-prod-step-type', {
     type: 'plugin',
@@ -16,20 +16,20 @@ Module.register('sg-prod-step-type', {
             component: 'sg-prod-step-type-index',
             path: 'index'
         },
-        // detail: {
-        //     component: 'sg-prod-step-type-detail',
-        //     path: 'detail/:id',
-        //     meta: {
-        //         parentPath: 'sg-prod-step-type-index'
-        //     }
-        // },
-        // create: {
-        //     component: 'sg-prod-step-type-create',
-        //     path: 'create',
-        //     meta: {
-        //         parentPath: 'sg-prod-step-type-index'
-        //     }
-        // }
+        detail: {
+            component: 'sg-prod-step-type-detail',
+            path: 'detail/:id',
+            meta: {
+                parentPath: 'sg-prod-step-type-index'
+            }
+        },
+        create: {
+            component: 'sg-prod-step-type-create',
+            path: 'create',
+            meta: {
+                parentPath: 'sg-prod-step-type-index'
+            }
+        }
     },
 
     navigation: [{
